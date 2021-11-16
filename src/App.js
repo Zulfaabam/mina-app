69a4b3b5a576129c1c28956d90f4c137e107f920
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-11-16 14:43:00 +0700

    example_axios

--- src/App.js
+++ src/App.js
@@ -12,8 +12,82 @@ import FormKonsultanPage from './components/FormKonsultanPage'
 import LoginKonsultanPage from './components/LoginKonsultanPage'
 
 // contoh ng kene lurr
+function ex amples(){
 
+  const axios = require("axios").default;
+
+  axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${localStorage.getItem(config.localStorage.dataToken)}`;
+  
+  axios
+        .get(config.urls.events)
+        .then((res) => {
+          let data = res.data;
+          this.events = data.data.reverse();
+        })
+        .catch((err) => {
+          if (err.response.status == 401) {
+            this.$router.push({ name: "Login" });
+          }
+        });
+  
+  axios({
+        method: "put",
+        url: config.urls.registerFace,
+        data: formData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then(() => {})
+        .catch((err) => {
+          callbacks.unauth(err.response.status, err.response.data.message);
+        });
+
+  
+  let formData = this.formDataCom;
+      formData.append("nama", this.nama);
+      formData.append("whatsapp", this.whatsapp);
+      formData.append("alamat", this.alamat);
+      formData.append("tanggal_lahir", this.tanggal_lahir);
+      formData.append("status_sekolah", this.status_sekolah);
+      formData.append("status_kerja", this.status_kerja);
+
+  let token = localStorage.getItem(config.localStorage.dataToken);
+  
+  axios({
+        method: "post",
+        url: config.urls.people,
+        data: formData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => {
+          if (res.status == 201) {
+            this.statusData = 201;
+          }
+        })
+        .catch((err) => {
+          if (err.response.status == 409) {
+            this.statusConflict = 409;
+            alert("WA terpakai!");
+          }
+          if (err.response.status == 400) {
+            let errors = err.response.data.messages;
+            this.errors = errors;
+          }
+          callbacks.unauth(err.response.status, err.response.data.message);
+        }); 
+  
+
+}
 // tekan kene
+
+
 
 function App() {
   return (
